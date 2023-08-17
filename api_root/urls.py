4e06d8ea67f429e4f1f5b026bcaf1f2fffdd0e9c
Guilherme dos santo souza <guilherme.souza@presidencia.gov.br>
2023-08-17 12:21:36 -0300

    adjust urls

--- api_root/urls.py
+++ api_root/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api_rest.urls'), name='api')
+    path('api/', include('api_rest.urls'), name='api'),
+    path('', include('api_rest.urls'), name='api')
 ]
